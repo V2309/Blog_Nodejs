4d1aef260cd30793a942c089c403e0f4a28cf6e4
V2309 <vanduongcr6@gmail.com>
2025-01-13 21:23:02 +0700

    step

---
+++ src/index.js
@@ -0,0 +1,26 @@
+const express = require('express') // Import thư viện express
+const morgan = require('morgan') // Import thư viện morgan
+const {engine} = require('express-handlebars') // Import thư viện express-handlebars
+const path = require('path') // Import thư viện path
+// port mà ứng dụng sẽ lắng nghe
+const port = 3000 
+const app = express() // Tạo một ứng dụng express
+// Sử dụng morgan để log request
+app.use(morgan('combined'))
+app.use(express.static(path.join(__dirname, 'public'))) // Định nghĩa thư mục chứa các file tĩnh như css, js, images
+
+// Sử dụng handlebars làm view engine
+app.engine('.hbs', engine({extname: '.hbs'})); // Định nghĩa handlebars engine với đuôi .hbs
+app.set('view engine', '.hbs') // Định nghĩa view engine là handlebars
+app.set('views',path.join(__dirname ,'resources/views'))// Định nghĩa thư mục chứa các view
+
+
+// Định nghĩa một route
+app.get('/', (req, res) => {
+  res.render('home')
+})
+
+// khởi chạy serve tại port 3000
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
