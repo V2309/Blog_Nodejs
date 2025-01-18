e2c1ee42703bd683e9707cbed67ab8563a4bd726
V2309 <vanduongcr6@gmail.com>
2025-01-18 21:42:18 +0700

    trash

--- src/app/controllers/MeController.js
+++ src/app/controllers/MeController.js
@@ -5,16 +5,30 @@ const { Compiler } = require('sass');
 const { Op } = require('sequelize');
 class MeController {
    // [GET] /me/stored/courses
-    storedCourses(req, res,next) {
-        Course.findAll({})
-            .then(courses => {
-                res.render('me/stored-courses', {
-                    courses: mutipleSequelizeToGet(courses),
-                });
-            })
-            .catch(next);
+ 
+storedCourses(req, res, next) {
+    Promise.all([
+        Course.count({ where: { deleted_at: { [Op.ne]: null } }, paranoid: false }), // Đếm các bản ghi đã xóa mềm
+        Course.findAll({ where: { deleted_at: null } }), // Lấy danh sách các bản ghi chưa bị xóa
+    ])
+        .then(([deletedCount, courses]) => {
+            res.render('me/stored-courses', {
+                courses: mutipleSequelizeToGet(courses),
+                deletedCount, // Truyền số lượng bản ghi đã xóa mềm vào view
+            });
+        })
+        .catch(next);
+}
+
+        // Course.findAll({})
+        //     .then(courses => {
+        //         res.render('me/stored-courses', {
+        //             courses: mutipleSequelizeToGet(courses),
+        //         });
+        //     })
+        //     .catch(next);
     
-    }
+    
    // [GET] /me/trash/courses
 trashCourses(req, res, next) {
     Course.findAll({
@@ -32,7 +46,8 @@ trashCourses(req, res, next) {
         })
         .catch(next);
 }
-
 }
+
+
 
 module.exports = new MeController(); // Export ra một instance của NewsController
